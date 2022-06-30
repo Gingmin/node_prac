f774d96264b93a55bf50477bff3ce33d31f9f419
gingmin <kkmk220@gmail.com>
2022-07-01 00:06:51 +0900

    22.07.01 commit

--- .idea/main/addCalculator.js
+++ .idea/main/addCalculator.js
@@ -4,6 +4,12 @@
 // }
 // console.log("모듈로 분리하기 전 - calc.add 함수 호출 결과 : %d", calc.add(10, 10));
 
-exports.add = function(a, b) {
+// exports.add = function(a, b) {
+//     return a + b;
+// }
+
+const calc = {};
+calc.add = function(a, b) {
     return a + b;
 }
+module.exports = calc;
